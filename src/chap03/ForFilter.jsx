884bf306601b87b48800621301bb35891d14dbf8
Yugo <mugiwara_yugo@icloud.com>
2026-08-28 15:14:59 -0400

    Update ForFilter.jsx. Paste the fragment

--- src/chap03/ForFilter.jsx
+++ src/chap03/ForFilter.jsx
@@ -5,10 +5,15 @@ export default functiom ForFilter ({ src }) {
     return (
         <dl>
             {lowPrice.map(elem => (
-                <Fragment key={elem.isbn}>
-                    <dt>
+               <Fragment key={elem.isbn}>
+                <dt>
+                    <a href={`https://wings.msn.to/books/${elem.isbn}/${elem.isbn}.jpg`}>
+                        {elem.title} ({elem.price} dollars)
+                    </a>
+                </dt>
+                <dd>{elem.summary}</dd>
                 </Fragment>
             ))}
-        </div>
-    )
+        </dl>
+    );
 }
