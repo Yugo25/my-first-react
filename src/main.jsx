b11a18fd6cfaf74499a88a9f04439ecab9654d4e
Yugo <mugiwara_yugo@icloud.com>
2026-08-28 15:42:59 -0400

    Update main.jsx. Add ForSort

--- src/main.jsx
+++ src/main.jsx
@@ -5,9 +5,11 @@ import App from './App.jsx'
 
 import EventBasic from './chap03/EventBasic';
 import StateBasic from './chap03/StateBasic';
+
 import books from './chap03/books.js';
 import ForList from './chap03/ForList.jsx';
 import ForFilter from './chap03/ForFilter.jsx';
+import ForSort from './chap03/ForSort.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
@@ -17,6 +19,7 @@ createRoot(document.getElementById('root')).render(
         <StateBasic init={0} />
         <ForList src={books} />
         <ForFilter src={books} />
+        <ForSort src={books} />
     </>
   </StrictMode>,
 )
