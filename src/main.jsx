2d6d483f7d1bc1bda0bc474133f8f3c6ae3faabf
Yugo <mugiwara_yugo@icloud.com>
2026-08-28 15:19:40 -0400

    Update main.jsx. Add ForFilter

--- src/main.jsx
+++ src/main.jsx
@@ -7,6 +7,7 @@ import EventBasic from './chap03/EventBasic';
 import StateBasic from './chap03/StateBasic';
 import books from './chap03/books.js';
 import ForList from './chap03/ForList.jsx';
+import ForFilter from './chap03/ForFilter.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
@@ -15,6 +16,7 @@ createRoot(document.getElementById('root')).render(
         <EventBasic type="time" />
         <StateBasic init={0} />
         <ForList src={books} />
+        <ForFilter src={books} />
     </>
   </StrictMode>,
 )
