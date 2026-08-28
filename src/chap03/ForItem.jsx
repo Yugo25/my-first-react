71aeafbb2adc029cbc0799658b7812955aae8add
Yugo <mugiwara_yugo@icloud.com>
2026-08-28 17:17:43 -0400

    Update ForItem.jsx

--- src/chap03/ForItem.jsx
+++ src/chap03/ForItem.jsx
@@ -1,4 +1,10 @@
 export default function ForItem({ book }) {
+    let dd;
+    if (book.download) {
+        dd = <dd>{book.summary}<Download isbn={book.isbn} /></dd>;
+    } else {
+        dd = <dd>{book.summary}</dd>;
+    }
     return (
         <>
             <dt>
@@ -6,7 +12,7 @@ export default function ForItem({ book }) {
                     {elem.title} ({elem.price} dollars)
                 </a>
             </dt>
-            <dd>{elem.summary}</dd>
+            {dd}
         </>
     );
 }
