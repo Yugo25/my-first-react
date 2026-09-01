f633b092ccc3a0fd377538c97d8d9330fcc6360a
Yugo <mugiwara_yugo@icloud.com>
2026-08-31 23:33:09 -0400

    Create FormList.jsx

---
+++ src/chap04/FormList.jsx
@@ -0,0 +1,41 @@
+import { useReact } from 'react';
+
+export default function FormList() {
+    const [form, setForm] = useState({
+        animal: ['dog', 'hamster']
+    });
+    
+    const handleFormList = e => {
+        const data = [];
+        
+        const ops = e.target.options;
+        for (const opt of options) {
+                if (opt.selected) {
+                    data.push(opt.value);
+            }
+        }
+        
+        setForm({
+            ...form,
+            [e.target.name]: data
+        });
+    };
+    
+    const show = () => {
+        console.log(`Favorite animal: ${form.animal}`);
+    };
+    
+    return ( 
+        <form>
+            <label htmlFor="animal">Favorite animal：</label><br/>
+            <select id="animal" name="animal"
+                value=(form.animal} size="4" multiple=(true} onChange=(handleFormList)>
+                <option value="dog">Dog</option>
+                <option value="cat">Cat</option>
+                <option value="hamster ">Hamster</option>
+                <option value="rabbit">Rabbit</option>
+            </select>
+            <button type="button" onClick={show}>Submit</button>
+        </form>
+    );
+}
