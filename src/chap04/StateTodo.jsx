1be58ef9a74d9d2cea8d80b7aad4db66edcd1279
Yugo25 <mugiwara_yugo@icloud.com>
2026-09-03 09:18:38 -0400

    task management

--- src/chap04/StateTodo.jsx
+++ src/chap04/StateTodo.jsx
@@ -1,11 +1,42 @@
 import { useState } from 'react';
+import './StateTodo.css';
 
 export default function StateTodo() {
+    const [maxId, setMaxId] = useState(1);
 
     const [title, setTitle] = useState('');
+    const [todo, setTodo] = useState([]);
 
     const handleChangeTitle = e => {
         setTitle(e.target.value);
+    };
+
+    const handleClick = () => {
+        if (title === '') return;
+
+        setTodo([
+            ...todo,
+            {
+                id: maxId,
+                title,
+                created: new Date(),
+                isDone: false,
+            }
+        ]);
+        setMaxId(maxId + 1);
+    }
+
+    const handleDone = e => {
+        setTodo(todo.map(item => {
+            if (item.id === Number(e.target.dataset.id)) {
+                return {
+                    ...item,
+                    isDone: true
+                };
+            } else {
+                return item;
+            }
+        }));
     };
 
     return (
@@ -14,6 +45,21 @@ export default function StateTodo() {
                 <input type="text" name="title"
                 value={title} onChange={handleChangeTitle} />
             </label>
+            <button type="button" 
+            onClick={handleClick}>Add</button>
+            <hr />
+
+            <ul>
+                {todo.map(item => (
+                    <li key={item.id}
+                    className={item.isDone ? 'done' : ''}>
+                        {item.title}
+                        <button type="button"
+                        onClick={handleDone} data-id={item.id}>Done
+                        </button>
+                    </li>
+                ))}
+            </ul>
         </div>
     );
 }
